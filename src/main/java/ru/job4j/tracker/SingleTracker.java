1b6c7f33c2d0b0d2ec2f4678fabe8488ec92cda1
vladimarus <cyberstalker@ya.ru>
2021-09-10 11:13:17 +0300

    2. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class SingleTracker {
+public final class SingleTracker {
     private static Tracker instance = null;
 
     public static Tracker getInstance() {
