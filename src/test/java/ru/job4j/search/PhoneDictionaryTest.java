5014dc8daf0c24610ba4152fc8da30ef8c850092
vladimarus <cyberstalker@ya.ru>
2021-09-24 09:03:50 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #207349]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
     @Test
@@ -16,5 +16,12 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByNameThenNone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        ArrayList<Person> persons = phones.find("Petr");
+        assertTrue(persons.isEmpty());
     }
 }
