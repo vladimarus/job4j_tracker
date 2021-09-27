49527de21396c07507ed3fa9ae89467d359ebc31
vladimarus <cyberstalker@ya.ru>
2021-09-27 16:40:45 +0300

    2. Модель данных. [#174683 #195768]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -3,9 +3,7 @@ package ru.job4j.oop;
 public class College {
     public static void main(String[] args) {
         Student student = new Student();
-        // повышающее приведение типа
         Enrollee enrollee = student;
-        // понижающее приведение типа
         student = (Student) enrollee;
     }
 }
