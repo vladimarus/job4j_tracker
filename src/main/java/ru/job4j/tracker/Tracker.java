6a7ecd49792977840e453ca0fd1b78a0af6829c0
vladimarus <cyberstalker@ya.ru>
2021-09-28 10:38:54 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039 #209103]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,7 +14,7 @@ public class Tracker {
     }
 
     public List<Item> findAll() {
-        return items;
+        return List.copyOf(items);
     }
 
     public List<Item> findByName(String key) {
@@ -48,7 +48,7 @@ public class Tracker {
         boolean result = index != -1;
         if (result) {
             item.setId(id);
-            items.add(index, item);
+            items.set(index, item);
         }
         return result;
     }
