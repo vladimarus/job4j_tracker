6eed6f240df82d680a68bca82bcafc86d9caee29
vladimarus <cyberstalker@ya.ru>
2021-12-09 14:31:45 +0300

    3. Checkstyle с Maven [#257606 #237666]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        List<String> rsl = new ArrayList<>();
+        return rsl;
+    }
+
+    public static void sortAsc(List<String> orgs) {
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+}
