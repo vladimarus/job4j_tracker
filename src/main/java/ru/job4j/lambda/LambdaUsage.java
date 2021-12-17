8afb809f364a7133cd7716f6cf156b84e0dfd5f3
vladimarus <cyberstalker@ya.ru>
2021-12-17 15:29:44 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Пример применения многострочного лямбда-блока
+ * Лямбдой реализован компаратор. Объеты Attachment сортируются по длине
+ * имени, в обратном порядке (по убыванию).
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 1.3.1", 120),
+                new Attachment("image 1.2", 23)
+        };
+
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            int res = Integer.compare(
+                    right.getName().length(),
+                    left.getName().length());
+            System.out.println(right.getName().length() + " < "
+                    + left.getName().length() + ": "
+                    + (res < 0));
+            return res;
+        };
+
+        Arrays.sort(atts, cmpDescSize);
+        System.out.println(Arrays.toString(atts));
+    }
+}
