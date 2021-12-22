8d765330e9ce19c0d011b3e6ece53e0b1001b350
vladimarus <cyberstalker@ya.ru>
2021-12-22 17:43:51 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#70609]

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -2,12 +2,18 @@ package ru.job4j.lambda;
 
 import java.util.function.Supplier;
 
+/**
+ * Пример взаимодейсвия лямбда-выражения с областью видимости.
+ * В данном случае для обеспечения безопасности параллелизма переменная @total
+ * не может быть захвачена лямбда-выражением. Для передачи @total в
+ * лямбда-выражение используется фактически-окончательная временная
+ * переменная @finalTotal
+ */
 public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = number[i];
+        for (int num : number) {
             int finalTotal = total;
             total = add(
                     () -> finalTotal + num
