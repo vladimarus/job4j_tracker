95f5fd0e07baa08e54139f68bee89db1c44beace
vladimarus <cyberstalker@ya.ru>
2021-12-22 16:07:18 +0300

    3. Ссылки на методы [#70608]

---
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        names.forEach(RefMethod::cutOut);
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
