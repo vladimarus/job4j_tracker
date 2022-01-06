62ef1c915b9424d9f933c7a8ce2468f929de7658
vladimarus <cyberstalker@ya.ru>
2022-01-07 00:05:30 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -10,8 +10,8 @@ import static org.junit.Assert.assertEquals;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertEquals(expect, result);
     }
@@ -19,7 +19,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertEquals(expect, result);
     }
@@ -27,7 +27,7 @@ public class DepartmentsTest {
     @Test
     public void whenAscOrder() {
         List<String> input = Arrays.asList("k1/sk2", "k2/sk1", "k1/sk1", "k2");
-        List<String> expect = Arrays.asList("k1/sk1", "k1/sk2", "k2", "k2/sk1");
+        List<String> expect = List.of("k1/sk1", "k1/sk2", "k2", "k2/sk1");
         Departments.sortAsc(input);
         assertEquals(expect, input);
     }
@@ -35,7 +35,7 @@ public class DepartmentsTest {
     @Test
     public void whenDescOrder() {
         List<String> input = Arrays.asList("k1/sk2", "k2/sk1", "k1/sk1", "k2");
-        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1/sk1", "k1/sk2");
+        List<String> expect = List.of("k2", "k2/sk1", "k1/sk1", "k1/sk2");
         Departments.sortDesc(input);
         assertEquals(expect, input);
     }
